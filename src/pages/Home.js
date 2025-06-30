3f3348bc5330f5995eda1bf99eb82803529c2451
Naveenrgukt225 <bhukyanaveennayak225@gmail.com>
2025-06-30 20:39:36 +0000

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/Home.css';
+
+function Home() {
+  return (
+    <div className="home-container">
+      <div className="home-hero">
+        <h1>Welcome to QuickKart 🛒</h1>
+        <h2>NaveNaik</h2>
+        <p>Your one-stop shop for everything you need</p>
+        <a  className="shop-now-button">Shop Now</a>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
